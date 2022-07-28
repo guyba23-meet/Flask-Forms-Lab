9b1b9e1c3df40f059e7d8eb340106c89ae55738a
guyba23-meet <109900022+guyba23-meet@users.noreply.github.com>
2022-07-28 15:17:03 +0300

    Not Done!

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -12,15 +12,25 @@ username = "llo2ay"
 password = "123"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
+@app.route('/home', methods = ['GET', 'POST'])  # '/' for the default page
+def home():
+	return render_template('home.html')
 
 @app.route('/', methods = ['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		user = request.form['username']
+		pas = request.form['password']
+		if user == username and pas == password:
+			return redirect(url_for('home'))
+		else:
+			return render_template('login.html')
 
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
-    debug=True
+		debug=True
 	)
